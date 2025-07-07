8f628ed56011a5208b4267dc036ccf983f01fa1c
DESKTOP-RGULR25\user1 <dkgus8551@gmail.com>
2025-07-07 09:14:00 +0900

    Spring 2일차 수업 내용

--- src/main/java/com/kh/spring/member/model/service/MemberServiceImpl.java
+++ src/main/java/com/kh/spring/member/model/service/MemberServiceImpl.java
@@ -1,5 +1,7 @@
 package com.kh.spring.member.model.service;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,34 +13,45 @@ public class MemberServiceImpl implements MemberService {
 
 	@Autowired
 	private MemberDao memberDao;
-
+	
+	@Override
+	public Member loginMember(Member m) {
+		
+		return memberDao.loginMember(m);
+	}
+	
 	@Override
 	public Member loginMember(String userId) {
-
+		
 		return null;
 	}
 
 	@Override
 	public int insertMember(Member m) {
 
-		return 0;
+		return memberDao.insertMember(m);
 	}
 
 	@Override
 	public int updateMember(Member m) {
 
-		return 0;
+		return memberDao.updateMember(m);
 	}
 
 	@Override
 	public int idCheck(String userId) {
 
-		return 0;
+		return memberDao.idCheck(userId);
 	}
 
 	@Override
 	public void updateMemberChagePwd() {
 
+	}
+
+	@Override
+	public HashMap<String, Object> selectOne(String userId) {
+		return memberDao.selectOne(userId);
 	}
 
 }
--- src/main/java/com/kh/spring/member/model/vo/Member.java
+++ src/main/java/com/kh/spring/member/model/vo/Member.java
@@ -1,4 +1,37 @@
 package com.kh.spring.member.model.vo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+//@Setter 
+//@Getter 
+@NoArgsConstructor 
+@AllArgsConstructor
+@Data
+@Builder
+//@ToString 
+//@EqualsAndHashCode
 public class Member {
 	
+	private int userNo;
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private String profileImg;
+	private String email;
+    private String birthday;
+    private String gender;
+    private String phone;
+    private String address;
+    private Date enrollDate;
+    private Date modifyDate;
+    private String status;
 }
